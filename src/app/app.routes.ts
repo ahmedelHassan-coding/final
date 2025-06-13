02451ed3a2df9b962cba276cda8d4c78594c401b
ahmedfathy <149051482+ahmedfathy-hub@users.noreply.github.com>
2025-06-13 06:47:58 +0100

    app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,53 @@
 import { Routes } from '@angular/router';
- import{StudentHomeComponent} from './student-home/student-home.component';
-import { HomeComponent } from './home/home.component';
+import{StudentHomeComponent} from './student-home/student-home.component';
+//import { HomeComponent } from './home/home.component';
+import { ErrorComponent } from './pages/error/error.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { HomeComponent } from './pages/home/home.component';
 import { JobDetailsComponent } from './job-details/job-details.component';
 import { JobApplicationComponent } from './job-application/job-application.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'studenthome', component: StudentHomeComponent },
-  { path: 'job/:id', component: JobDetailsComponent },
-  { path: 'apply/:id', component: JobApplicationComponent },
+  { 
+      path:'',
+      redirectTo:'home',
+      pathMatch:'full'
+  },
+  {
+        path:'home',
+        component:HomeComponent
 
-  { path: '**', component: HomeComponent },
+  },
+  {
+        path:'login',
+        component:LoginComponent
+
+  },
+  {
+        path:'signup',
+        component:SignupComponent
+
+  },
+  { 
+    path: 'studenthome', 
+    component: StudentHomeComponent
+  },
+  { 
+    path: 'job/:id', 
+    component: JobDetailsComponent
+  },
+  { 
+    path: 'apply/:id', 
+    component: JobApplicationComponent },
+
+  { 
+    path:'**',
+    component:ErrorComponent
+  },
 ];
+
+
+
+
+
