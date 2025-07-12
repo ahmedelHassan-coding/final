8b51a88a1e36494dc1d6065918330b37b0f6b6f4
ahmedfathy <149051482+ahmedfathy-hub@users.noreply.github.com>
2025-07-12 20:02:39 +0100

    jobcontrol.component.ts

--- src/app/pages/jobcontrol/jobcontrol.component.ts
+++ src/app/pages/jobcontrol/jobcontrol.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-jobcontrol',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './jobcontrol.component.html',
   styleUrl: './jobcontrol.component.css'
 })
