b6994ccbfe9c1ef34be73759ee76a7940eef25f1
ahmedfathy <149051482+ahmedfathy-hub@users.noreply.github.com>
2025-07-12 19:54:20 +0100

    companypreview.component.ts

--- src/app/pages/companypreview/companypreview.component.ts
+++ src/app/pages/companypreview/companypreview.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-companypreview',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './companypreview.component.html',
   styleUrl: './companypreview.component.css'
 })
