5cfe5deae668eefedd2e22e75df70ea1b2bd2dac
youssef642 <youssefaboseif72@gmail.com>
2025-07-22 04:07:36 +0300

    added stripe payment

--- src/app/pages/payment/payment.component.ts
+++ src/app/pages/payment/payment.component.ts
@@ -1,11 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { loadStripe, Stripe, StripeCardElement } from '@stripe/stripe-js';
+import { environment } from '../../../environments/environment';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-payment',
-  imports: [],
   templateUrl: './payment.component.html',
-  styleUrl: './payment.component.css'
+  styleUrls: ['./payment.component.css'],
+  imports: [FormsModule, ReactiveFormsModule, CommonModule, RouterModule],
 })
-export class PaymentComponent {
+export class PaymentComponent implements OnInit {
+  stripe: Stripe | null = null;
+  cardElement!: StripeCardElement;
+  loading = false;
+  amount = 0; // قيمة الدفع
+  applicationId!: string; // ID من التطبيق
+  successMessage = '';
+  errorMessage = '';
 
+  constructor(private http: HttpClient, private route: ActivatedRoute) {
+    this.applicationId = this.route.snapshot.params['id'];
+  } 
+
+  async ngOnInit() {
+    this.stripe = await loadStripe(environment.stripePublicKey);
+
+    const elements = this.stripe!.elements();
+    this.cardElement = elements.create('card');
+    this.cardElement.mount('#card-element');
+  }
+
+  async handlePayment() {
+    this.loading = true;
+    this.errorMessage = '';
+    this.successMessage = '';
+
+    const { paymentMethod, error } = await this.stripe!.createPaymentMethod({
+      type: 'card',
+      card: this.cardElement,
+    });
+
+    if (error) {
+      this.errorMessage = error.message ?? 'payment failed';
+      this.loading = false;
+      return;
+    }
+
+    // إرسال إلى Laravel
+    this.http
+      .post(`${environment.apiUrl}/pay`, {
+        amount: this.amount,
+        application_id: this.applicationId,
+        payment_method_id: paymentMethod.id,
+      })
+      .subscribe({
+        next: (res: any) => {
+          this.successMessage = 'payment success';
+          this.loading = false;
+        },
+        error: (err) => {
+          this.errorMessage = err.error?.error || 'payment failed';
+          this.loading = false;
+        },
+      });
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  stripePublicKey: 'pk_test_51MxIQkIByg1EjjpJo0VTuPyNBsF0xBFhQjOujsjdhrnQyIv7K1HgBNoQGHNSJ4H43WN5fvDb62T5owRo3USk8qB900q3CpfRgy', // مفتاح النشر من Stripe
+  apiUrl: 'http://localhost:8000/api' // رابط الـ Laravel API
+};
